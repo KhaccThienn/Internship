9ca6b7d5754d9049089d159069bd8a423bdd811a
Lee Tien Sinh <muzankibut112@gmail.com>
2022-09-13 15:33:10 +0700

    Summary

---
+++ src/Components/Pages/Home/Components/Carousel/Carousel.js
@@ -0,0 +1,42 @@
+import classNames from "classnames/bind";
+import Carousel from "framer-motion-carousel";
+import { Link } from "react-router-dom";
+import style from "./carousels.module.css";
+
+const cx = classNames.bind(style)
+const image = [
+  "https://wpbingosite.com/wordpress/wrish/wp-content/uploads/2021/10/slider1.jpg",
+  "https://wpbingosite.com/wordpress/wrish/wp-content/uploads/2021/10/slider2.jpg",
+  "https://wpbingosite.com/wordpress/wrish/wp-content/uploads/2021/10/slider3.jpg",
+];
+
+const styles = {
+  height: 600,
+}
+
+const Carousels = () => (
+  <div>
+    <Carousel renderDots={false} style={styles}>
+      {image.map((item, i) => (
+        <div className={cx("position-relative")}>
+          <img
+            draggable={false}
+            src={item}
+            alt="..."
+            key={i}
+            className="card-img"
+          />
+          <div className={cx("overlays", "text-white")}>
+            <p>Timeless & Elegant</p>
+            <h1>Best Seller</h1>
+            <p>
+              Complete your everyday look with a classic leather strap watch.
+            </p>
+            <Link to={"/"} className={cx("btn-pages")}>Shop Now</Link>
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  </div>
+);
+export default Carousels;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import style from "./home.module.css"
+import Carousels from './Components/Carousel/Carousel'
 const cx = classNames.bind(style)
 function Home() {
   return (
-    <div className={cx('text-danger', "text-center")}>
+    <div className={cx('text-danger', "container-fluid")}>
       home
+      <Carousels />
     </div>
   )
 }
