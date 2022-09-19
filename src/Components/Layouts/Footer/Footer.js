7e9d50d1d57b676b84fcb4d74815f0bce43d861f
Nguyễn Thị Kim Yến <yenn0750@gmail.com>
2022-09-19 08:46:16 +0700

    commit to Yen

--- src/Components/Layouts/Footer/Footer.js
+++ src/Components/Layouts/Footer/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import classNames from 'classnames/bind';
+import style from './Footer.module.css';
+
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+
+    </div>
   )
 }
 
