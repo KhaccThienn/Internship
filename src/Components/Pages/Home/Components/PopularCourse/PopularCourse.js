b27b81a7f82def350eccbf14a3c1f85adc38f5a2
Lee Tien Sinh <muzankibut112@gmail.com>
2022-10-01 08:35:55 +0700

    add data

--- src/Components/Pages/Home/Components/PopularCourse/PopularCourse.js
+++ src/Components/Pages/Home/Components/PopularCourse/PopularCourse.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import classNames from 'classnames/bind'
-import styled from "./popular.module.css"
-import home1 from "~/Asset/home1.png";
-import home2 from "~/Asset/home2.png";
-import home3 from "~/Asset/home3.png";
-import home4 from "~/Asset/home4.png";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import classNames from "classnames/bind";
+import styled from "./popular.module.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const cx = classNames.bind(styled)
+const cx = classNames.bind(styled);
 
 function PopularCourse() {
+  const [course, setCourse] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:9999/thumpCourse").then((res) => {
+      console.log(course);
+      setCourse(res.data);
+    });
+  }, []);
+
   return (
     <div className={cx("container")}>
       <div className={cx("home-popular", "mt-4")}>
@@ -17,63 +22,28 @@ function PopularCourse() {
           Những Khóa Học Phổ Biến Nhất
         </h4>
         <div className="row">
-          <div className="col-lg-3 col-md-6 col-sm-12">
-            <img src={home1} alt="" className="card-img" />
-            <p className={cx("home-level", "mt-2")}>Basic</p>
-            <p className="text-center font-weight-bold home-text">
-              Khóa Học Kiến Thức Nền Tảng
-            </p>
-            <p>By: LuanNV.</p>
-            <Link to="" className="btn btn-outline-dark text-center home-btn">
-              Học Ngay!
-            </Link>
-          </div>
-
-          <div className="col-lg-3 col-md-6 col-sm-12">
-            <img src={home2} alt="" className="card-img" />
-            <p className={cx("home-level", "mt-2")}>Advance</p>
-            <p className="text-center font-weight-bold home-text text-truncate">
-              Khóa Học Responsive Web Design
-            </p>
-            <p>By: LuongNA.</p>
-            <Link to="" className="btn btn-outline-dark text-center home-btn">
-              Học Ngay!
-            </Link>
-          </div>
-
-          <div className="col-lg-3 col-md-6 col-sm-12">
-            <img src={home3} alt="" className="card-img" />
-            <p className={cx("home-level", "mt-2")}>Advance</p>
-            <p className="text-center font-weight-bold home-text">
-              Khóa Học Javascript Nâng Cao
-            </p>
-            <p>By: YenNTK.</p>
-            <Link
-              to="/upcomming"
-              className="btn btn-outline-dark text-center home-btn"
-            >
-              Học Ngay!
-            </Link>
-          </div>
-
-          <div className="col-lg-3 col-md-6 col-sm-12">
-            <img src={home4} alt="" className="card-img" />
-            <p className={cx("home-level", "mt-2")}>Advance</p>
-            <p className="text-center font-weight-bold home-text">
-              Khóa Học WSL / Ubuntu
-            </p>
-            <p>By: HauNV.</p>
-            <Link
-              to="/upcomming"
-              className="btn btn-outline-dark text-center home-btn"
-            >
-              Học Ngay!
-            </Link>
-          </div>
+          {course.map((items, index) => {
+            return (
+              <div className="col-lg-3 col-md-6 col-sm-12" key={index}>
+                <img src={items.img} alt="" className="card-img" />
+                <p className={cx("home-level", "mt-2")}>{items.level}</p>
+                <p className={cx("text-center", "font-weight-bold", "home-text", "text-truncate")}>
+                  {items.name}
+                </p>
+                <p>By: { items.author }</p>
+                <Link
+                  to=""
+                  className="btn btn-outline-dark text-center home-btn"
+                >
+                  Học Ngay!
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
 }
 
-export default PopularCourse
+export default PopularCourse;
