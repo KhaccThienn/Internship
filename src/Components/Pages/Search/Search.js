e62723ee930af657ac88b72a3c3bab5235c79c9e
Hautrung1608 <hautrung16082003@gmail.com>
2022-10-05 10:02:38 +0700

    adadadadad

--- src/Components/Pages/Search/Search.js
+++ src/Components/Pages/Search/Search.js
@@ -1,32 +1,54 @@
-import { Button } from "antd";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames/bind";
-import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
-import { GrMenu } from "react-icons/gr";
-import { Link } from "react-router-dom";
-import logo from "~/Asset/logo.png";
 import style from "./search.module.css";
+import axios from "axios";
+
 
 let cx = classNames.bind(style);
-
-
 function Search() {
+
+  const [course, setCourse] = useState([]);
+  const [dCourse, setDCourse] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9999/course")
+      .then((res) => {
+        console.log(course);
+        setCourse(res.data);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       <h2 className="ketqua">Kết quả tìm kiếm cho : JavaScript</h2>
-
+      {/* {course.map((items, index) => {
+        { console.log(items.name) }
+      })} */}
       <div className="container-fruid">
         <div className={cx("row", "p-0")}>
-          <div className={cx("card", "col-lg-2", "card-timkiem")} >
-            <img src="Data/image/java.png" className={cx("card-img-top", "p-0")} alt="..." />
-            <div className={cx("card-body")}>
-              <h5 className={cx("card-title")}>JavaScript cơ bản</h5>
-              <p className={cx("card-text")}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className={cx("btn", "btn-danger", "bt-card")}>Xem Ngay</a>
-            </div>
-          </div>
+          {course.map((items, index) => {
+            return (
+
+              <div className={cx("card", "col-lg-2", "card-timkiem")} >
+                <div className={cx("card-img-top", "p-0", "card-imgne")}>{items.url}</div>
+                <div className={cx("card-body")}>
+                  <h5 className={cx("card-title","text-truncate")}>{items.name}</h5>
+                  <p className={cx("card-text", "text-truncate")}>{items.maintain}</p>
+                  <a href="#" className={cx("btn", "btn-danger", "bt-card")}>Xem Ngay</a>
+                </div>
+              </div>
+
+            );
+
+          })}
         </div>
       </div>
+
     </div>
   );
 }
